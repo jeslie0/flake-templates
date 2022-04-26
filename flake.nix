f369183c9297c0ae01b00b742245a0090dbc7cc0
James Leslie <jamesleslie@posteo.net>
2022-04-26 01:31:03 +0100

    [latex] Added latex template
    Truly reproducible latex template
    
    Made template better

--- flake.nix
+++ flake.nix
@@ -3,6 +3,22 @@
 
   outputs = { self, ... }: {
     templates = {
+      C = { path = ./C;
+            description = "A very basic C project, with devshell";
+          };
+
+      Cpp = { path = ./Cpp;
+              description = "A very basic C++ project, with devshell";
+            };
+
+      Csharp = { path = ./Csharp;
+                 description = "A very basic C# project, with devshell";
+               };
+
+      hakyll = { path = ./Hakyll/hakyll-init;
+                 description = "A new Hakyll site, generated from hakyll-init";
+               };
+
       haskell-new-template = { path = ./Haskell/HaskellNew;
                                description = "Flake template for a general Haskell project";
                              };
@@ -11,25 +27,14 @@
                                  description = "Add a flake.nix to an existing Haskell project";
                                };
 
-      hakyll = { path = ./Hakyll/hakyll-init;
-                 description = "A new Hakyll site, generated from hakyll-init";
-               };
+      latex = { path = ./latex;
+                description = "A very basic LaTeX project, with a devshell";
+              };
 
       python = { path = ./Python;
-                 description = "A very basic Python project with a devshell";
-            };
-
-      C = { path = ./C;
-            description = "A very basic C project, with devshell";
-          };
-
-      Csharp = { path = ./Csharp;
-                 description = "A very basic C# project, with devshell";
+                 description = "A very basic Python project, with a devshell";
                };
 
-      Cpp = { path = ./Cpp;
-              description = "A very basic C++ project, with devshell";
-          };
     };
   };
 }
---
+++ latex/flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "A LuaTex project flake template";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs.james-fonts.url = "github:jeslie0/my-fonts-flake";
+  inputs.texmf.url = "github:jeslie0/texmf";
+
+  outputs = { self, nixpkgs, flake-utils, james-fonts, texmf }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        myfonts = james-fonts.defaultPackage.${system};
+        tex = pkgs.texlive.combine { # Put the packages that we want texlive to use when compiling the PDF in here.
+          inherit (pkgs.texlive)
+            # scheme-minimal
+            # scheme-basic
+            # scheme-small
+            # scheme-medium
+            scheme-full
+            latex-bin
+            fontspec
+            latexmk;
+        };
+        buildInputs = [ pkgs.coreutils tex myfonts texmf];
+        packageName = throw "EnterPackageName";
+      in
+        {
+          packages.${packageName} = pkgs.stdenvNoCC.mkDerivation {
+            pname = packageName;
+            version = "0.0.1";
+            buildInputs = buildInputs;
+            src = ./src;
+            phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+            buildPhase = ''
+                       export PATH="${pkgs.lib.makeBinPath buildInputs}";
+                       mkdir -p .cache/texmf-var
+                       env TEXMFHOME=${texmf} \
+                           TEXMFVAR=.cache/texmf-var \
+                           OSFONTDIR=${myfonts}/share/fonts \
+                           SOURCE_DATE_EPOCH=${toString self.lastModified} \
+                             latexmk -interaction=nonstopmode -pdf -lualatex -bibtex \
+                             -pretex="\pdfvariable suppressoptionalinfo 512\relax" \
+                             $src/main.tex
+                         '';
+            installPhase = ''
+                           mkdir -p $out
+                           cp main.pdf $out/${packageName}.pdf
+                           '';
+          };
+
+          defaultPackage = self.packages.${system}.${packageName};
+
+          devShell = pkgs.mkShell {
+            packages = with pkgs; [ texlab ];
+            inputsFrom = [ self.packages.${system}.${packageName} ];
+
+          };
+        }
+    );
+}
